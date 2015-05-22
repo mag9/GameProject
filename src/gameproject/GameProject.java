dad4ead85c9f88906560bee9bb27a5a2efd6261a
mag9 <razestudiosdev@gmail.com>
2015-05-22 08:27:47 -0400

    Created Project

---
+++ src/gameproject/GameProject.java
@@ -0,0 +1,18 @@
+package gameproject;
+
+/**
+ *
+ * @author jeremystark
+ */
+public class GameProject
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
