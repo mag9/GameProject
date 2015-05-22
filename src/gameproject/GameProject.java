1eeabcc3bfd05c8153bec41d5cd54e74179cfad8
mag9 <razestudiosdev@gmail.com>
2015-05-22 08:57:24 -0400

    Added new JFrame

--- src/gameproject/GameProject.java
+++ src/gameproject/GameProject.java
@@ -1,18 +1,29 @@
 package gameproject;
 
+import javax.swing.*;
+
 /**
  *
- * @author jeremystark, blackalek,The Meowseph Stalin, mrMcMonigle, and Cingel
+ * @author jeremystark, blackalek,The Meowseph Stalin, mrMcMonigle, and Cingel.
  */
 public class GameProject
 {
 
+    private String title = "Game!";
+    private int width = 800;
+    private int height = 600;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setResizable(false);
+        f.setSize(800, 600);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
     }
     
 }
