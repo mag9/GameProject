0633be8d0e237a5c09f4620bf0baf84eb9758fdf
mag9 <razestudiosdev@gmail.com>
2015-05-22 09:00:47 -0400

    Moved JFrame to constructor.

--- src/gameproject/GameProject.java
+++ src/gameproject/GameProject.java
@@ -16,14 +16,21 @@ public class GameProject
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
+    
+    public GameProject()
     {
         JFrame f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setResizable(false);
-        f.setSize(800, 600);
+        f.setTitle(title);
+        f.setSize(width, height);
         f.setLocationRelativeTo(null);
         f.setVisible(true);
+    }
+    
+    public static void main(String[] args)
+    {
+        GameProject gp = new GameProject();
     }
     
 }
