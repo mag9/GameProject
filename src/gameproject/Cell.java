08a8e49f5f8a54d8907fd86eda2b0a60edc4a4d3
17mfleisher <17mfleisher@OHSCAD11.orange.local>
2015-05-26 08:40:39 -0400

    Added basic Cell class

---
+++ src/gameproject/Cell.java
@@ -0,0 +1,21 @@
+package gameproject;
+
+/**
+ *
+ * @author blackalek
+ */
+public class Cell
+{
+    int x, y, radius;
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+    
+}
