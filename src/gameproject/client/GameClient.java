5cd353208c4723e38925ef4365a1246dbe71ca18
Ryan Rule-Hoffman <ry60003333@mac.com>
2015-05-28 14:00:04 -0400

    Added networking packages.

---
+++ src/gameproject/client/GameClient.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameproject.client;
+
+/**
+ *
+ * @author Ryan Rule-Hoffman <ryan.rulehoffman@icloud.com>
+ */
+public class GameClient
+{
+    
+}
---
+++ src/gameproject/server/ServerEngine.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameproject.server;
+
+/**
+ * The engine of the server.
+ * @author Ryan Rule-Hoffman <ryan.rulehoffman@icloud.com>
+ */
+public abstract class ServerEngine
+{
+    
+}
